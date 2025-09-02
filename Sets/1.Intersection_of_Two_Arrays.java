ea0f60d5a171d9accf00127f6f60722954ba4d1e
MD SAHIL KHAN <159130157+SAHIL-4112004@users.noreply.github.com>
2025-09-02 23:29:06 +0530

    02Sep

---
+++ Sets/1.Intersection_of_Two_Arrays.java
@@ -0,0 +1,25 @@
+/**
+ * Problem: Intersection of Two Arrays (leetcode 349)
+ * Link: https://leetcode.com/problems/intersection-of-two-arrays/submissions/1757332605/
+ * Approach : Sets
+ */
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for(int n : nums1) set1.add(n);
+        for(int n : nums2) set2.add(n);
+
+        set1.retainAll(set2);
+
+        int[] arr = new int[set1.size()];
+        int i=0;
+        for(int n : set1){
+            arr[i++] = n;
+        }
+
+        return arr;
+    }
+}
